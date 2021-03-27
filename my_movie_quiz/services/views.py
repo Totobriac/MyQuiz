a0d6892f38aa220f3ff30448caf819cc0514f360
Totobriac <totobriac@gmail.com>
2021-03-27 18:44:09 +0100

    trailer done :)

--- my_movie_quiz/services/views.py
+++ my_movie_quiz/services/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 import subprocess
 import json
+import urllib.parse
 from .models import Tags, Picture
 from .serializers import PictureSerializer, TagsSerializer
 from rest_framework.decorators import api_view
@@ -22,40 +23,42 @@ class MoviesSearch(View):
             json_data = json.loads(response.text)
         
             for i in json_data["results"]:
-                print(i)
                 if any(j in i["genre_ids"] for j in [16, 99, 10402, 10770]) or i["vote_count"] < 150:
                     pass
                 else: movies_list.append({"id": i["id"],
                                           "poster":i["poster_path"],
                                           "title":i["original_title"],
                                           "backdrop":i["backdrop_path"],
-                                          "year":i["release_date"]})
+                                          "year":i["release_date"],
+                                          "plot":i["overview"],
+                                          "trailer":""})
 
             if len(json_data["results"]) < 20:
                 isSearching = False
             else: index += 1
+        for i in movies_list:
+            search_url = "https://api.themoviedb.org/3/movie/" + str(i['id']) +"/credits?api_key=137e8b3a07e487eeeaa6f211207f674a&language=en-US"
+            response = requests.get(search_url)
+            json_data = json.loads(response.text)
+            i['cast'] = json_data['cast'][:4]
 
         return JsonResponse(movies_list, safe=False)
-            
 
-class MovieSearch(View):
 
-    def get(self, request, movieId):
+class TrailerSearch(View):
 
-        search_url = "https://api.themoviedb.org/3/movie/" + str(movieId) + "/external_ids?api_key=137e8b3a07e487eeeaa6f211207f674a"
+    def get(self, request, *args, **kwargs):
+        title = kwargs.get('title')
+        date = kwargs.get('date').split("-")[0]
+        params = urllib.parse.quote(title + " hd trailer " + date)        
+        search_url = "https://youtube.googleapis.com/youtube/v3/search?part=id&maxResults=1&q=" + params  + "&key=AIzaSyDdU8fgEmwuJP-eL3v_HxNv2AogCmC7wYA"
         response = requests.get(search_url)
         json_data = json.loads(response.text)
-        imdb_id = json_data["imdb_id"] 
+        yt_id = json_data["items"][0]["id"]["videoId"]
 
-        search_url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + imdb_id
-        headers = {
-            'x-rapidapi-key': "3c90722e1fmshd3624c65becfb47p1d49a9jsn722fc9909dc4",
-            'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com"
-            }
-        response = requests.get( search_url, headers=headers)
-        return HttpResponse(response)
+        return JsonResponse({'trailer_id': yt_id}, safe=False)
 
-        
+
 class ActorsSearch(View):
 
     def get(self, request, actors):
@@ -74,14 +77,6 @@ class ActorsSearch(View):
                 i += 1
             actorPicUrl.append(url_list)
         return JsonResponse(actorPicUrl, safe=False)
-
-
-class TrailerSearch(View):
-    
-    def get(self, request, trailer_id):
-        video_src = subprocess.run(["youtube-dl", "-g", "https://www.imdb.com/videoplayer/" + trailer_id], encoding='utf-8', stdout=subprocess.PIPE)
-        print(video_src)
-        return JsonResponse(video_src.stdout, safe=False)
 
 
 class PictureViewSet(viewsets.ModelViewSet):   
