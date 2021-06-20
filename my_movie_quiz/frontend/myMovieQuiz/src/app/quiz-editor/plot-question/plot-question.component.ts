ea402e7cefa8ed4b6ef1334f05e8a632a457771e
totobriac <totobriac@gmail.com>
2021-06-20 02:50:58 +0200

    q/a animation

--- my_movie_quiz/frontend/myMovieQuiz/src/app/quiz-editor/plot-question/plot-question.component.ts
+++ my_movie_quiz/frontend/myMovieQuiz/src/app/quiz-editor/plot-question/plot-question.component.ts
@@ -2,24 +2,42 @@ import { Component, OnInit, } from '@angular/core';
 import { MovieDataService } from "../../services/movie-data.service";
 import { Subscription } from 'rxjs';
 import { MovieDb } from 'src/app/interfaces/movie';
-import { PlotTools} from 'src/app/interfaces/plotTools';
+import { PlotTools } from 'src/app/interfaces/plotTools';
 import { PlotToolsDataService } from 'src/app/services/plotTools-data.service';
+import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-plot-question',
   templateUrl: './plot-question.component.html',
-  styleUrls: ['./plot-question.component.css']
+  styleUrls: ['./plot-question.component.css'],
+  animations: [
+    trigger('flyingTool', [
+      transition(':enter', [
+        animate('1s', keyframes([
+          style({ transform: 'translateX(-100%)', opacity: '0', offset: 0}),
+          style({ transform: 'translateX(10%)', opacity: '1', offset: 0.8}),
+          style({ transform: 'translateX(0%)', opacity: '1', offset: 1.0})
+        ]))
+      ]),
+      transition(':leave', [        
+        animate('800ms', keyframes([
+          style({ transform: 'translateX(-10%)', opacity: '1', offset: 0.3}),
+          style({ transform: 'translateX(100%)', opacity: '0', offset: 1.0})
+        ]))
+      ])
+    ]
+    )]
 })
 
 export class PlotQuestionComponent implements OnInit {
-  
+
   editable = "false";
   movie: MovieDb;
   tools: PlotTools;
   subscription: Subscription;
 
   constructor(private movieData: MovieDataService,
-              private plotTools: PlotToolsDataService) { }
+    private plotTools: PlotToolsDataService) { }
 
 
   ngOnInit(): void {
@@ -27,12 +45,12 @@ export class PlotQuestionComponent implements OnInit {
     this.subscription = this.plotTools.currentPlotTools.subscribe(tools => this.tools = tools);
     console.log(this.tools.card);
   }
- 
+
   enableEdition() {
-    this.editable == "false" ?  this.editable = "true" : this.editable = "false"
+    this.editable == "false" ? this.editable = "true" : this.editable = "false"
   }
 
   shuffle() {
-    this.movie.plot = this.movie.plot.split(' ').sort(() => Math.floor(Math.random() * Math.floor(3)) - 1).join(' ').replace(".","") + "."
+    this.movie.plot = this.movie.plot.split(' ').sort(() => Math.floor(Math.random() * Math.floor(3)) - 1).join(' ').replace(".", "") + "."
   }
 }
